b593530f14d577d4c87dcdf0334e515b6b6afb4c
VladimirYanovskiy <117820316+VladimirYanovskiy@users.noreply.github.com>
2022-11-25 08:58:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 tickets = int(input("Введите количество приобретаемых билетов: "))
 person = 1
 
+b0 = 0.00 # Билет бесплатный 0,00 руб.
+b1 = 990.00 # Билет 990,00 руб.
+b3 = 1390.00 # Билет 1390,00 руб.
+
 sum = 0.00
 while person <= tickets:
     age_for_ticket = int(input(f"Укажите взраст посетителя, для которого приобретается билет № {person} ? "))
     if age_for_ticket < 18:
-        print("Билет бесплатный: 0 руб.")
+        print(f"Билет бесплатный: {b0} руб.")
     elif 25 > age_for_ticket >= 18:
-        sum += 990.00
-        print("Стоимость билета: 990 руб.")
+        sum = sum + b1
+        print(f"Стоимость билета: {b1} руб.")
     else:
-        sum += 1390.00
-        print("Стоимость билета: 1390 руб.")
-    person += 1
+        sum = sum + b3
+        print(f"Стоимость билета: {b3} руб.")
+    person = person + 1
 
 if tickets > 3:
     sale = sum * 0.9
